5226fc476c4cd6286d88ed70bad0b207e016d948
gam0022 <gam0022@gmail.com>
2016-07-31 20:33:28 +0900

    Add RaymarchingObject(WIP)

---
+++ raymarching_object.h
@@ -0,0 +1,61 @@
+﻿#ifndef	_RAYMARCHINNG_OBJECT_H_
+#define	_RAYMARCHINNG_OBJECT_H_
+
+#include <cmath>
+
+#include "vec.h"
+#include "ray.h"
+#include "material.h"
+#include "constant.h"
+#include "intersection.h"
+
+namespace edupt {
+
+struct RaymarchingObject {
+	static const int kREP = 64;
+
+	Color emission;
+	Color color;
+	ReflectionType reflection_type;
+
+	RaymarchingObject(const Color &emission, const Color &color, const ReflectionType reflection_type) :
+	  emission(emission), color(color), reflection_type(reflection_type) {}
+
+	float distanceFunction(const Vec &position) const {
+		return (position - Vec(65, 20, 20)).length() - 15.0;
+	}
+
+	Vec calcNormal(const Vec &position) const {
+		return normalize(Vec(
+			distanceFunction(position + Vec( kEPS, 0.0, 0.0 ) ) - distanceFunction(position + Vec( -kEPS, 0.0, 0.0 ) ),
+			distanceFunction(position + Vec( 0.0, kEPS, 0.0 ) ) - distanceFunction(position + Vec( 0.0, -kEPS, 0.0 ) ),
+			distanceFunction(position + Vec( 0.0, 0.0, kEPS ) ) - distanceFunction(position + Vec( 0.0, 0.0, -kEPS ) )
+		));
+	}
+
+	// 入力のrayに対する交差点までの距離を返す。交差しなかったら0を返す。
+	// rayとの交差判定を行う。交差したらtrue,さもなくばfalseを返す。
+	bool intersect(const Ray &ray, Hitpoint *hitpoint) const {
+		float dist;
+		float depth = 0.0;
+		Vec p = ray.org;
+		for (int i = 0; i < kREP; i++) {
+			dist = distanceFunction(p);
+			depth += dist;
+			p = ray.org + depth * ray.dir;
+			if (abs(dist) < kEPS) break;
+		}
+
+		if (abs(dist) < kEPS) {
+			hitpoint->position = p;
+			hitpoint->normal = calcNormal(p);
+			return true;
+		} else {
+			return false;
+		}
+	}
+};
+
+};
+
+#endif
--- scene.h
+++ scene.h
@@ -3,6 +3,7 @@
 
 #include "constant.h"
 #include "sphere.h"
+#include "raymarching_object.h"
 #include "intersection.h"
 
 namespace edupt {
@@ -15,10 +16,14 @@ const Sphere spheres[] = {
 	Sphere(1e5, Vec(50, 40.8, -1e5+250), Color(),      Color(),                 REFLECTION_TYPE_DIFFUSE), // 手前
 	Sphere(1e5, Vec(50, 1e5, 81.6),      Color(),      Color(0.75, 0.75, 0.75), REFLECTION_TYPE_DIFFUSE), // 床
 	Sphere(1e5, Vec(50, -1e5+81.6, 81.6),Color(),      Color(0.75, 0.75, 0.75), REFLECTION_TYPE_DIFFUSE), // 天井
-	Sphere(20,Vec(65, 20, 20),           Color(),      Color(0.25, 0.75, 0.25), REFLECTION_TYPE_DIFFUSE), // 緑球
-	Sphere(16.5,Vec(27, 16.5, 47),       Color(),      Color(0.99, 0.99, 0.99), REFLECTION_TYPE_SPECULAR), // 鏡
-	Sphere(16.5,Vec(77, 16.5, 78),       Color(),      Color(0.99, 0.99, 0.99), REFLECTION_TYPE_REFRACTION), //ガラス
+	//Sphere(20,Vec(65, 20, 20),           Color(),      Color(0.25, 0.75, 0.25), REFLECTION_TYPE_DIFFUSE), // 緑球
+	//Sphere(16.5,Vec(27, 16.5, 47),       Color(),      Color(0.99, 0.99, 0.99), REFLECTION_TYPE_SPECULAR), // 鏡
+	//Sphere(16.5,Vec(77, 16.5, 78),       Color(),      Color(0.99, 0.99, 0.99), REFLECTION_TYPE_REFRACTION), //ガラス
 	Sphere(15.0,Vec(50.0, 90.0, 81.6),   Color(36,36,36), Color(),              REFLECTION_TYPE_DIFFUSE), //照明
+};
+
+const RaymarchingObject objects[] = {
+	RaymarchingObject(Color(), Color(0.75, 0.25, 0.25), REFLECTION_TYPE_DIFFUSE),
 };
 
 // シーンとの交差判定関数
@@ -33,6 +38,16 @@ inline bool intersect_scene(const Ray &ray, Intersection *intersection) {
 	for (int i = 0; i < int(n); i ++) {
 		Hitpoint hitpoint;
 		if (spheres[i].intersect(ray, &hitpoint)) {
+			if (hitpoint.distance < intersection->hitpoint.distance) {
+				intersection->hitpoint = hitpoint;
+				intersection->object_id = i;
+			}
+		}
+	}
+
+	for (int i = 0; i < 1; i++) {
+		Hitpoint hitpoint;
+		if (objects[i].intersect(ray, &hitpoint)) {
 			if (hitpoint.distance < intersection->hitpoint.distance) {
 				intersection->hitpoint = hitpoint;
 				intersection->object_id = i;
