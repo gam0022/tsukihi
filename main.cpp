08467fdb610794aa973e0da46434c8116c5e9407
gam0022 <gam0022@gmail.com>
2016-08-05 00:44:06 +0900

    Added time measurement

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 ﻿#define _CRT_SECURE_NO_DEPRECATE
 #include <iostream>
+#include <time.h>
 
 #include "render.h"
 
 int main(int argc, char **argv) {
 	std::cout << "Path tracing renderer: edupt" << std::endl << std::endl;
+	clock_t start = clock();
 
 	// 640x480の画像、(2x2) * 4 sample / pixel
 	edupt::render(640 / 2, 480 / 2, 1000, 1);
+
+	clock_t end = clock();
+	std::cout << "Total time: " << (double)(end - start) / CLOCKS_PER_SEC << "sec." << std::endl;
 }
